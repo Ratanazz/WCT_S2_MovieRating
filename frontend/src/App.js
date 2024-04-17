c4cf65db0e4b0b1af07582178c2d35db78b42ae2
Ratana <rathanachan55@gmail.com>
2024-04-17 12:09:22 +0700

    Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbarr from './components/Navbarr';
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Navbarr/>
+      {/* <Route exact path="/" component={Home} /> */}
+    </Router>
   );
 }
 
