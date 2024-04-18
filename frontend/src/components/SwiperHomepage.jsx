1bd71ad674caf8e323a2cd6f27c0ded21f1c5e56
Ratana <rathanachan55@gmail.com>
2024-04-19 00:09:27 +0700

    homechange2

--- frontend/src/components/SwiperHomepage.jsx
+++ frontend/src/components/SwiperHomepage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Css/SwiperHomepageCss.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { Swiper, SwiperSlide } from 'swiper/react'; // Import Swiper components
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -19,11 +21,17 @@ function SwiperHomepage() {
         className="mySwiper"
       >
         <SwiperSlide>
-          <div className="Cardd">
+          <div className="Card">
             <div className="infomation">
               <h1>Spiderman</h1>
               <div className="info1">
                 <h2>Runtime: 2h 12min</h2><h2>Rated: PG13</h2><h2>Release: dd mm yyyy</h2>
+              </div>
+              <div className="rate">
+              <div className="star-icon">
+                <FontAwesomeIcon icon={faStar} style={{ color: "#FFD43B" }} />
+              </div>
+                <h3>7.4/5</h3>
               </div>
             </div>
             <div className="Poster">
@@ -31,7 +39,25 @@ function SwiperHomepage() {
             </div>
           </div>
         </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>
+        <div className="Card">
+            <div className="infomation">
+              <h1>KungFu Panda</h1>
+              <div className="info1">
+                <h2>Runtime: 1h 12min</h2><h2>Rated: PG13</h2><h2>Release: dd mm yyyy</h2>
+              </div>
+              <div className="rate">
+              <div className="star-icon">
+                <FontAwesomeIcon icon={faStar} style={{ color: "#FFD43B" }} />
+              </div>
+                <h3>7.4/5</h3>
+              </div>
+            </div>
+            <div className="Poster">
+              <img src="assets/pandaposter.jpg" alt="" />
+            </div>
+          </div>
+        </SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
         
