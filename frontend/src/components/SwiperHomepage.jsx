4ad200f2f8fec52338839c6871abdc84474f19ac
Ratana <rathanachan55@gmail.com>
2024-04-21 12:09:56 +0700

    updatehomepage

--- frontend/src/components/SwiperHomepage.jsx
+++ frontend/src/components/SwiperHomepage.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
 import './Css/SwiperHomepageCss.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
@@ -9,6 +11,16 @@ import 'swiper/css/pagination';
 import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
 
 function SwiperHomepage() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await axios.get('http://127.0.0.1:8000/movies'); // Replace with your API endpoint
+      setMovies(response.data);
+    };
+
+    fetchMovies();
+  }, []);
   return (
     <div className='SwiperHomepage'>
       <Swiper
@@ -20,46 +32,27 @@ function SwiperHomepage() {
         modules={[Navigation, Pagination, Mousewheel, Keyboard]}
         className="mySwiper"
       >
-        <SwiperSlide>
-          <div className="Card">
-            <div className="infomation">
-              <h1>Spiderman</h1>
-              <div className="info1">
-                <h2>Runtime: 2h 12min</h2><h2>Rated: PG13</h2><h2>Release: dd mm yyyy</h2>
-              </div>
-              <div className="rate">
-              <div className="star-icon">
-                <FontAwesomeIcon icon={faStar} style={{ color: "#FFD43B" }} />
-              </div>
-                <h3>7.4/5</h3>
-              </div>
-            </div>
-            <div className="Poster">
-              <img src="assets/spidermanposter.jpg" alt="" />
-            </div>
+        {movies.map(movie => (
+  <SwiperSlide key={movie.id}>
+    <div className="Card">
+      <div className="infomation">
+        <h1>{movie.name}</h1>
+        <div className="info1">
+          <h2>Runtime: {movie.runtime}</h2><h2>Release: {movie.releaseDate}</h2>
+        </div>
+        <div className="rate">
+          <div className="star-icon">
+            <FontAwesomeIcon icon={faStar} style={{ color: "#FFD43B" }} />
           </div>
-        </SwiperSlide>
-        <SwiperSlide>
-        <div className="Card">
-            <div className="infomation">
-              <h1>KungFu Panda</h1>
-              <div className="info1">
-                <h2>Runtime: 1h 12min</h2><h2>Rated: PG13</h2><h2>Release: dd mm yyyy</h2>
-              </div>
-              <div className="rate">
-              <div className="star-icon">
-                <FontAwesomeIcon icon={faStar} style={{ color: "#FFD43B" }} />
-              </div>
-                <h3>7.4/5</h3>
-              </div>
-            </div>
-            <div className="Poster">
-              <img src="assets/pandaposter.jpg" alt="" />
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
+          <h3>{movie.rating}/5</h3>
+        </div>
+      </div>
+      <div className="Poster">
+        <img src={movie.image_poster} alt="" />
+      </div>
+    </div>
+  </SwiperSlide>
+))}
         
       </Swiper>
     </div>
